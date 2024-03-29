911cf1e6dbd7920303c93e9b17eaec4d6b8fdd93
Xeviousbr <xeviousbr@gmail.com>
2024-03-29 20:10:12 -0300

    Primeiro commit do YouTubsonsIA

---
+++ Form1.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+
+//O objetivo deste programa é mostrar vídeos de YouTube
+//E ele irá já mostrar os filmes de acordo com uma lista de canais de uma base
+//O usuário irá dar notas nos vídeos, que serão a própria operação do usuário
+//A IA deverá aprender porque o usuário aprovou ou não o vídeo
+//E assim ir melhorando as sugestões 
+
+namespace YouTubsonsIA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
